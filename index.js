c3609c08b576735f8d6991cd0b046193a83b66c5
Matija Janjic <matijanjic@gmail.com>
2021-11-24 11:57:46 +0000

    uibuild

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+require('dotenv').config()
 const { request, response } = require('express')
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
+const Person = require('./models/person')
+
 
 app.use(express.static('build'))
 app.use(cors())
@@ -10,44 +13,26 @@ app.use(express.json())
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(people => {
+        response.json(people)
+    })
 })
 
 app.get('/api/info', (request, response) => {
     date = new Date()
     response.send(`
-    <p>Phonebook has info for ${persons.length} people.</p>
+    <p>Phonebook has info for people.</p>
     <p>${date}</p>
     `)
 })
 
 app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
+    const person = Person.findById(request.params.id).then(person => {
+        response.json(person)
+    })
 
     if (person) {
         response.json(person)
@@ -57,36 +42,38 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
+    
+    Person.deleteOne({"_id": `ObjectId(${request.params.id})`})
     response.status(204).end()
 })
 
-const generateId = () => {
-    return Math.floor(Math.random() * 1000)
-}
-
 app.post('/api/persons', (request, response) => {
     const body = request.body
-    const person = {
-        id: generateId(),
+    console.log(request.body)
+
+    if (!body) {
+        return response.status(400).json({
+            error: "content missing."
+        })
+    }
+
+    const person = new Person({
         name: body.name,
         number: body.number
-    }
-    if (!person.name) {
-        return response.status(400).json({error: "name missing."})
-    } else if (!person.number) {
-        return response.status(400).json({error: "number missing."})
-    } else if (persons.some(p => p.name === person.name)) {
-        return response.status(400).json({error: "name already in phonebook."})
-    } else {
-
-    persons = persons.concat(person)
-    response.json(person)
+    })
     
-    }
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
+    
 })
 
-const PORT = process.env.PORT || 3001
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
